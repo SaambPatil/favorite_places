08c8187e82ee55296b73a82b588c3e27ef0be7b7
saambpatil@outlook.com <saambpatil@outlook.com>
2024-07-21 14:53:16 +0530

    Added places list and add place logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
-// ignore_for_file: deprecated_member_use
-
-import 'package:favorite_places/screens/places.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-final colorScheme = ColorScheme.fromSeed( 
+import 'package:favorite_places/screens/places.dart';
+
+final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
   seedColor: const Color.fromARGB(255, 102, 6, 247),
   background: const Color.fromARGB(255, 56, 49, 66),
 );
 
-
-
-
 final theme = ThemeData().copyWith(
-  scaffoldBackgroundColor: colorScheme.surface,
+  useMaterial3: true,
+  scaffoldBackgroundColor: colorScheme.background,
   colorScheme: colorScheme,
   textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
     titleSmall: GoogleFonts.ubuntuCondensed(
@@ -32,7 +29,7 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-      const MyApp(),
+    const ProviderScope(child: MyApp()),
   );
 }
 
--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,5 +1,7 @@
+import 'package:favorite_places/screens/place_detail.dart';
 import 'package:flutter/material.dart';
-import 'package:favorite_places/models/places.dart';
+
+import 'package:favorite_places/models/place.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
@@ -11,23 +13,30 @@ class PlacesList extends StatelessWidget {
     if (places.isEmpty) {
       return Center(
         child: Text(
-          'No Places Added Yes',
-          style: Theme.of(context)
-              .textTheme
-              .bodyLarge!
-              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          'No places added yet',
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
         ),
       );
     }
 
     return ListView.builder(
-      itemBuilder: (context, index) => ListTile(
+      itemCount: places.length,
+      itemBuilder: (ctx, index) => ListTile(
         title: Text(
           places[index].title,
           style: Theme.of(context).textTheme.titleMedium!.copyWith(
                 color: Theme.of(context).colorScheme.onBackground,
               ),
         ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailScreen(place: places[index]),
+            ),
+          );
+        },
       ),
     );
   }
